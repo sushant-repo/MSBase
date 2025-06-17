1bda4bcd9e123d30e5e5d0c699cef94f740bace0
Sushant <sus.hant@outlook.com>
2025-06-17 16:42:33 +1000

    Sets up DBUp project

---
+++ DatabaseUpdates/Program.cs
@@ -0,0 +1,33 @@
+﻿using DbUp;
+using System.Reflection;
+
+
+var connectionString = "Data Source=(local);Initial Catalog=MSBase;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=True";
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var upgrader =
+        DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
